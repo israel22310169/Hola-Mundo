c7775de411bc095bf678e9a7ca347bf6e04be7d4
israel22310169 <159966207+israel22310169@users.noreply.github.com>
2024-02-20 00:06:11 +0000

    version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class Alimento
+{
+    private:
+        int Energia;
+    public:
+    Alimento(int Energia)
+    {
+        this->Energia = Energia;
+    }
+    int ExtraerEnergia()
+    {
+        return this->Energia;
+    }
+    ~Alimento(){}
+};
